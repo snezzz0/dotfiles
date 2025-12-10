8c977e13a992bcd775f55ecfeb2b2e33eb030d47
snezzz0 <sn33zyhub@gmail.com>
2025-12-10 16:16:49 +0100

    waybar update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 new dotfile repo optimized for gnu stow
+
+Waybar Credit : [minimal-wm-config](https://github.com/mubin-thinks/minimal-wm-config)
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,2 @@
-if test (tty) = "/dev/tty1"
-  exec Hyprland
-end
-
-if status is-interactive
-    fastfetch
-end
 set -x STARSHIP_CONFIG ~/.config/starship.toml
 starship init fish | source
