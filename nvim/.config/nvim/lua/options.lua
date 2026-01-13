06acd4db2d8d916f1e5c157d0d3489750e793e5d
snezzz0 <sn33zyhub@gmail.com>
2026-01-13 12:50:04 +0100

    fixed missing blur

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -2,6 +2,8 @@ require "nvchad.options"
 vim.opt.number = true
 vim.opt.relativenumber = true
 -- add yours here!
+vim.opt.timeoutlen = 400
+vim.opt.ttimeoutlen = 10
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
