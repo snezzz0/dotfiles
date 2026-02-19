e1d57f9b604cb2326d244fdf744fd224662adeb4
snezzz0 <sn33zyhub@gmail.com>
2026-02-20 00:55:56 +0100

    latest

--- archived/nvim-old-nvchad/.config/nvim/lua/plugins/markdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    enabled = true,
-    ft = { "markdown" }, 
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    opts = {},
-  }
-}
